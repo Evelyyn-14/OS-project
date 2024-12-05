7ec2ab8a9beca7a603f10e8067629240ee59a68a
evelyn.e <eescobedo1@student.gsu.edu>
2024-12-05 18:48:44 -0500

    added main class

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,14 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String filename = "example.txt";
-        String directory = ".";
-
-        FileOperations.createFile(filename);
-        FileOperations.writeFile(filename, "Hello, this is a test file!");
-        FileOperations.readFile(filename);
-        FileOperations.searchFile(directory, filename);
-        FileOperations.viewFileMetadata(filename);
-        FileOperations.deleteFile(filename);
-        FileOperations.searchFile(directory,filename);
+        MenuHandler menuHandler = new MenuHandler();
+        menuHandler.displayMenu();
     }
 }
